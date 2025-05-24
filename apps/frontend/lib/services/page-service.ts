54094bad9bc0d74b2aa5513e64ce3e1f774e0b7c
nreyes-stratum-it <nreyes@stratum-it.com>
2025-05-24 08:36:19 -0600

    wip: find-by-slug content endpoint created, not implemented yet

--- apps/frontend/lib/services/page-service.ts
+++ apps/frontend/lib/services/page-service.ts
@@ -1,6 +1,5 @@
 import {PageProvider} from "@/lib/interfaces/providers/page";
 import {PagesData} from "@/lib/types/pages/pages";
-import {LocalizationData} from "@/lib/types/localizations/localization";
 
 export class PageService implements PageProvider {
     constructor(private readonly pageProvider: PageProvider) {
@@ -11,7 +10,4 @@ export class PageService implements PageProvider {
         return this.pageProvider.getPageBySlug(slug, locale);
     }
 
-    async getPageLanguageVariants(slug: string, locale: string): Promise<LocalizationData[] | null> {
-        return this.pageProvider.getPageLanguageVariants(slug, locale);
-    }
 }
